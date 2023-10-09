31b0b2dcb1023002725cb351f01ec43ef6bd96f1
jinseo0315 <kjsys12173@gmail.com>
2023-10-09 09:29:15 +0900

    1. about: br간격 수정, footer 적용 2. exhibition: photo 섹션

--- recruit/src/components/ExhibitionPhoto.js
+++ recruit/src/components/ExhibitionPhoto.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ExhibitionPhoto.css';
 import '../App.css'
+import Footer from './Footer';
 
 function ExhibitionPhoto() {
   return ( 
@@ -11,6 +12,7 @@ function ExhibitionPhoto() {
       <div className='photo-img'>
         <img src="image/photo.png" className="photo" alt="photo"/>
       </div>
+    <Footer />
     </div>
   );
 }
--- recruit/src/components/pages/About.js
+++ recruit/src/components/pages/About.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../../App.css';
 import './About.css';
+import Footer from '../Footer';
 
 function About() {
 
@@ -55,21 +56,28 @@ function About() {
             <div className='sub-heading'>
               기획</div>
             <div className='name'>
+            <div className="br-gap1" style={{ marginBottom: '5px' }} />
             컴퓨터공학과 17 김태현</div>
             <div className='sub-heading'>
               디자인</div>
             <div className='name'>
+            <div className="br-gap1" style={{ marginBottom: '5px' }} />
             스마트ICT융합공학과 18 김현경</div>
 
            {/* 프론트*/}
            <div className='sub-title'>
-            [프론트엔드]</div>
+            [프론트엔드]
+            <div className="br-gap1" style={{ marginBottom: '22px' }} /> 
+            </div>
             <div className='name'>
-            <br className="br-gap1"/>
-            국제무역학과 19 김지흔<br/>
-            스마트ICT융합공학과 20 김지현<br />
-            경영학과 21 신준용<br />
-            국제무역학과 21 김진서<br /></div>
+              국제무역학과 19 김지흔
+              <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+              스마트ICT융합공학과 20 김지현
+              <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+              경영학과 21 신준용
+              <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+              국제무역학과 21 김진서
+            </div>
 
             {/* 백엔드*/}
           <div className='sub-title'>
@@ -77,26 +85,37 @@ function About() {
             <div className='sub-heading'>
               JAVA</div>
             <div className='name'>
-            융합생명공학과 17 정제원<br/>
-            컴퓨터공학과 17 최병찬<br/>
-            컴퓨터공학과 18 곽민재<br />
-            컴퓨터공학과 20 강찬욱<br />
-            컴퓨터공학과 21 정연채<br />
+            <div className="br-gap1" style={{ marginBottom: '5px' }} />
+            융합생명공학과 17 정제원
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+            컴퓨터공학과 17 최병찬
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+            컴퓨터공학과 18 곽민재
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+            컴퓨터공학과 20 강찬욱
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+            컴퓨터공학과 21 정연채
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
             컴퓨터공학과 21 강민희<br />
             </div>
             <div className='sub-heading'>
              PYTHON</div>
             <div className='name'>
-            스마트ICT융합공학과 19 이홍석<br />
-            컴퓨터공학과 20 손수빈<br />
-            컴퓨터공학과 21 김정은<br />
+            <div className="br-gap1" style={{ marginBottom: '5px' }} /> 
+            스마트ICT융합공학과 19 이홍석
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+            컴퓨터공학과 20 손수빈
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
+            컴퓨터공학과 21 김정은
+            <div className="br-gap1" style={{ marginBottom: '3px' }} /> 
             경영학과 23 노선재<br />
             </div>
         </div>
-      <div className='source'>
-        <p>아이콘 출처<br />
-        Flaticon.com</p>
-      </div>
+        <div className='source'>
+          <p>아이콘 출처<br />
+          Flaticon.com</p>
+        </div>
+        <Footer />
       </div>
   );
 }
