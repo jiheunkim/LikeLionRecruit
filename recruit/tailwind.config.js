a646f4875b96881727c41eef1c4aa0062215f2bd
cheerjihyeon <gimjihyeon111@gmail.com>
2023-11-15 01:48:34 +0900

    refactor : 작업 내용 및 feature/jinnns2 병합

--- recruit/tailwind.config.js
+++ recruit/tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '2.6875rem',
+      'xl': '12px',
+      'full': '9999px',
+      'large': '12px',
+    }
   },
   plugins: [],
 }
